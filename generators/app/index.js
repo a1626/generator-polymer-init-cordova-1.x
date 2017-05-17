9dce2b54ae73f7a0a8013131e71cbf4d5b721e10
a1626 <gupta.abhishek1804@gmail.com>
2017-05-17 16:01:46 +0530

    adding options

--- generators/app/index.js
+++ generators/app/index.js
@@ -11,13 +11,67 @@ module.exports = class extends Generator {
     ));
 
     const prompts = [{
-      type: 'confirm',
-      name: 'someAnswer',
-      message: 'Would you like to enable this option?',
-      default: true
-    }];
+      type: 'input',
+      name: 'projectName',
+      message: 'Project Name?',
+      default: this.appname
+  },{
+    type: 'input',
+    name: 'elementName',
+    message: 'shell Name?'
+  },{
+    type: 'input',
+    name: 'packageName',
+    message: 'Cordova package?',
+    store: true,
+    default: 'org.apache.cordova.'+this.appname
+},{
+  type: 'confirm',
+  name: 'cordovaConfig',
+  message: 'Would you like to configure cordova?',
+  default: true
+},{
+    when: (response) => {
+        return response.cordovaConfig
+    },
+    name: 'description',
+    message: "Description (This same description will be used for bower and package.json)?",
+    default: 'A sample Apache Cordova application that responds to the deviceready event.'
+},{
+    when: (response) => {
+        return response.cordovaConfig
+    },
+    type: 'input',
+    name: 'authors',
+    message: "authors (This same description will be used for bower and package.json)?",
+    default: 'Apache Cordova Team'
+},{
+    when: (response) => {
+        return response.cordovaConfig
+    },
+    type: 'input',
+    name: 'authorEmail',
+    message: "author\'s email (This same description will be used for bower and package.json)?",
+    default: 'dev@cordova.apache.org'
+},{
+    when: (response) => {
+        return response.cordovaConfig
+    },
+    type: 'input',
+    name: 'authorHref',
+    message: "author\'s href (This same description will be used for bower and package.json)?",
+    default: 'http://cordova.io'
+},{
+    when: (response) => {
+        return response.cordovaConfig
+    },
+    type: 'checkbox',
+    name: 'platforms',
+    message: "Platforms:",
+    default: 'http://cordova.io'
+}];
 
-    return this.prompt(prompts).then(props => {
+    return this.prompt(prompts, function (response) {}).then(props => {
       // To access props later use this.props.someAnswer;
       this.props = props;
     });
