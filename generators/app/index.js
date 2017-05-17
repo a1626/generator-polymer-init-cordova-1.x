4d82c7d3989ee9e177d5626f12ae0e626a78c68f
a1626 <gupta.abhishek1804@gmail.com>
2017-05-17 16:45:12 +0530

    fixing #1

--- generators/app/index.js
+++ generators/app/index.js
@@ -15,63 +15,79 @@ module.exports = class extends Generator {
       name: 'projectName',
       message: 'Project Name?',
       default: this.appname
-  },{
-    type: 'input',
-    name: 'elementName',
-    message: 'shell Name?'
-  },{
-    type: 'input',
-    name: 'packageName',
-    message: 'Cordova package?',
-    store: true,
-    default: 'org.apache.cordova.'+this.appname
-},{
-  type: 'confirm',
-  name: 'cordovaConfig',
-  message: 'Would you like to configure cordova?',
-  default: true
-},{
-    when: (response) => {
+    },{
+      type: 'input',
+      name: 'elementName',
+      message: 'shell Name?'
+    },{
+      type: 'input',
+      name: 'packageName',
+      message: 'Cordova package?',
+      store: true,
+      default: 'org.apache.cordova.'+this.appname
+    },{
+      type: 'confirm',
+      name: 'cordovaConfig',
+      message: 'Would you like to configure cordova?',
+      default: true
+    },{
+      when: (response) => {
         return response.cordovaConfig
-    },
-    name: 'description',
-    message: "Description (This same description will be used for bower and package.json)?",
-    default: 'A sample Apache Cordova application that responds to the deviceready event.'
-},{
-    when: (response) => {
+      },
+      name: 'description',
+      message: "Description (This same description will be used for bower and package.json)?",
+      default: 'A sample Apache Cordova application that responds to the deviceready event.'
+    },{
+      when: (response) => {
         return response.cordovaConfig
-    },
-    type: 'input',
-    name: 'authors',
-    message: "authors (This same description will be used for bower and package.json)?",
-    default: 'Apache Cordova Team'
-},{
-    when: (response) => {
+      },
+      type: 'input',
+      name: 'authors',
+      message: "authors (This same description will be used for bower and package.json)?",
+      default: 'Apache Cordova Team'
+    },{
+      when: (response) => {
         return response.cordovaConfig
-    },
-    type: 'input',
-    name: 'authorEmail',
-    message: "author\'s email (This same description will be used for bower and package.json)?",
-    default: 'dev@cordova.apache.org'
-},{
-    when: (response) => {
+      },
+      type: 'input',
+      name: 'authorEmail',
+      message: "author\'s email (This same description will be used for bower and package.json)?",
+      default: 'dev@cordova.apache.org'
+    },{
+      when: (response) => {
         return response.cordovaConfig
-    },
-    type: 'input',
-    name: 'authorHref',
-    message: "author\'s href (This same description will be used for bower and package.json)?",
-    default: 'http://cordova.io'
-},{
-    when: (response) => {
+      },
+      type: 'input',
+      name: 'authorHref',
+      message: "author\'s href (This same description will be used for bower and package.json)?",
+      default: 'http://cordova.io'
+    },{
+      when: (response) => {
         return response.cordovaConfig
-    },
-    type: 'checkbox',
-    name: 'platforms',
-    message: "Platforms:",
-    default: 'http://cordova.io'
-}];
+      },
+      type: 'checkbox',
+      name: 'platforms',
+      message: "Platforms:",
+      choices: [{
+        name: 'android',
+        checked: true
+      },{
+        name: 'ios',
+        checked: true
+      },{
+        name: 'blackberry10'
+      },{
+        name: 'browser'
+      },{
+        name: 'firefoxos'
+      },{
+        name: 'osx'
+      },{
+        name: 'webos'
+      }]
+    }];
 
-    return this.prompt(prompts, function (response) {}).then(props => {
+    return this.prompt(prompts).then(props => {
       // To access props later use this.props.someAnswer;
       this.props = props;
     });
